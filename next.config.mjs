e42fda4d9d659d7ccc8cd8e2823c731fc1620807
Rivenshell <rivenshell111@gmail.com>
2023-09-26 16:11:03 -0400

    development phase/ phase I

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: { domains: ["discordapp.com"] },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Button } from "./Button";
+import { ProfileImage } from "./Profileimage";
+import { useSession } from "next-auth/react";
 
 export default function NewPostForm() {
+  const sesson = useSession();
+
+  if (sesson.status !== "authenticated") return;
+
   return (
     <>
       <form className="flex flex-col gap-2 border-b px-6 py-2">
         <div className="flex gap-4">
-          {/* <ProfileImage src='#url' /> */}
+          <ProfileImage src={sesson.data.user} />
           {/* form text and profile image goes here */}
           <textarea
             className="flex-grow resize-none overflow-hidden outline-none"
             placeholder="Rave, estatic. Type here ⚡️"
           />
         </div>
-        <Button>Post</Button>
+        <Button className=" self-end">Post</Button>
       </form>
     </>
   );
